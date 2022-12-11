68ce57cee3948d17c62b516cf901ab5935b5661c
ryoma ishizu <dara.ryouma@gmail.com>
2022-12-12 01:28:48 +0900

    add vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true
 })
+module.exports = {
+    pwa: {
+        workboxOptions: {
+            exclude: [/\.map$/, /web\.config$/],
+        },
+    },
+};
